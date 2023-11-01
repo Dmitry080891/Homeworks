ac7b3f55044a2c3b8821c3b585add2639a9bb4df
derevyanko_de <derevyanko.dm.ev@gmail.com>
2023-11-02 01:10:26 +0300

    add homework02 Task3

---
+++ Homework/src/ru/innopolis/java/Homework02/Task3.java
@@ -0,0 +1,18 @@
+package ru.innopolis.java.Homework02;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Введите исходную строку: ");
+        String str = scanner1.nextLine();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Сколько раз вывести строку? ");
+        int number = scanner2.nextInt();
+        int i = 0;
+        while (i<number) {
+            System.out.print(str);
+            i++;
+        }
+    }
+}
