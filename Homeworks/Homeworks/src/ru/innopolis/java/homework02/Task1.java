70239b56687e75326cbedc2082f1fbf68b77a788
derevyanko_de <derevyanko.dm.ev@gmail.com>
2023-11-03 21:24:40 +0300

    Homework02 add Task1

---
+++ Homeworks/Homeworks/src/ru/innopolis/java/homework02/Task1.java
@@ -0,0 +1,12 @@
+package ru.innopolis.java.homework02;
+
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите температуру в градусах по Фарингейту: ");
+        double tempF = scanner.nextDouble();
+        double tempC = (tempF - 32)*5/9;
+        System.out.println("Градусы по Цельсию: " + tempC);
+    }
+}
