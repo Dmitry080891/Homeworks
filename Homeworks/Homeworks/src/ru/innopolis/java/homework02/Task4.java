b5a29cbaa76e2fb11ea1de216e9de829bfa0804f
derevyanko_de <derevyanko.dm.ev@gmail.com>
2023-11-03 21:28:23 +0300

    Homework02 add Task4

---
+++ Homeworks/Homeworks/src/ru/innopolis/java/homework02/Task4.java
@@ -0,0 +1,18 @@
+package ru.innopolis.java.homework02;
+
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Ввидите число строк и столбцов в сетке: ");
+        int number = scanner1.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Введите исходную строку: ");
+        String str = scanner2.nextLine();
+        String[][] arr = new String[number][number];
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                arr[i][j] = str;
+                System.out.print(arr[i][j]+ " ");            }
+            System.out.println();   }
+    }    }
