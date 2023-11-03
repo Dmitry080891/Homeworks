488c5edc8f08367a872aeb502cf92e0887309f43
derevyanko_de <derevyanko.dm.ev@gmail.com>
2023-11-03 21:21:05 +0300

    Homework01 add Task1

---
+++ Homeworks/Homeworks/src/ru/innopolis/java/homework01/Task1.java
@@ -0,0 +1,19 @@
+package ru.innopolis.java.homework01;
+
+import java.util.Random;
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите минимальное значение: ");
+        int start = scanner.nextInt();
+        System.out.print("Введите максимальное значение: ");
+        int end = scanner.nextInt();
+        Random random = new Random();
+        System.out.print("Вывод четырех случайных чисел: ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print((random.nextInt(end-start)+start) + " ");
+        }
+
+    }
+}
