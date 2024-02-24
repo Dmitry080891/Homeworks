2f398cc63b9a540af5549a47d5af62710b121dd8
derevyanko_de <derevyanko.dm.ev@gmail.com>
2024-02-25 00:41:12 +0300

    Create

---
+++ ThridAttestation/ThirdAttestation/src/main/java/ru/innopolis/thirdattestation/ThirdAttestationApplication.java
@@ -0,0 +1,13 @@
+package ru.innopolis.thirdattestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdAttestationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdAttestationApplication.class, args);
+    }
+
+}
